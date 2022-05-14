399872ecc497fb3fa33894c9338609ec0bd50249
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-14 23:33:57 +0300

    This commit creates managershell to create shell context

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-# from flask import Flask
+from flask import Flask
 from app import create_app,db
+from flask_script import Manager, Server
 from app.models import User
 
 # from flask_script import Manager,Server
@@ -7,12 +8,12 @@ from app.models import User
 
 
 
-# # Creating app instance
-# app = create_app('development')
-# # app = create_app('test')
+# Creating app instance
+app = create_app('development')
+app = create_app('test')
 
-# manager = Manager(app)
-# manager.add_command('server',Server)
+manager = Manager(app)
+manager.add_command('server',Server)
 
 # # migrate = Migrate(app,db)
 # # manager.add_command('db',MigrateCommand)
@@ -25,20 +26,16 @@ from app.models import User
 # #     tests = unittest.TestLoader().discover('tests')
 # #     unittest.TextTestRunner(verbosity=2).run(tests)
 
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
+# app = Flask(__name__)
+
+# @app.route('/')
+# def test():
+#     return "Hello"
 
 # if __name__ == '__main__':
-#     manager.run()
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def test():
-    return "Hello"
-
-if __name__ == '__main__':
-    app.run()
+#     app.run()
