e42abc25c38038841d079f4865161bbfc219c64c
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-14 22:45:56 +0300

    This commit imports the db instance and the class user

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 # from flask import Flask
-# from app import create_app
+from app import create_app,db
+from app.models import User
+
 # from flask_script import Manager,Server
 # # from  flask_migrate import Migrate, MigrateCommand
 
-# # from app.models import User
 
 
 # # Creating app instance
