6f20352e64202f8a4b01f309fe783c14a0e3a9de
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-14 17:20:50 +0300

    This commit creates a virtual environment

--- manage.py
+++ manage.py
@@ -1,32 +1,43 @@
+# from flask import Flask
+# from app import create_app
+# from flask_script import Manager,Server
+# # from  flask_migrate import Migrate, MigrateCommand
+
+# # from app.models import User
+
+
+# # Creating app instance
+# app = create_app('development')
+# # app = create_app('test')
+
+# manager = Manager(app)
+# manager.add_command('server',Server)
+
+# # migrate = Migrate(app,db)
+# # manager.add_command('db',MigrateCommand)
+
+
+# # @manager.command
+# # def test():
+# #     """Run the unit tests."""
+# #     import unittest
+# #     tests = unittest.TestLoader().discover('tests')
+# #     unittest.TextTestRunner(verbosity=2).run(tests)
+
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app)
+
+# if __name__ == '__main__':
+#     manager.run()
+
 from flask import Flask
-from app import create_app
-from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
 
-# from app.models import User
+app = Flask(__name__)
 
-
-# Creating app instance
-app = create_app('development')
-# app = create_app('test')
-
-manager = Manager(app)
-manager.add_command('server',Server)
-
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
-
-
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app)
+@app.route('/')
+def test():
+    return "Hello"
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
