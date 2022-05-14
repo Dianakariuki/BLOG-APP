fffca5797a5431b87b68fe7cd96611f5de7cc3da
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-14 23:46:43 +0300

    This commit imports the Post,Comment and Post like

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app import create_app,db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User,Post,Comment,PostLike
+
 
 # from flask_script import Manager,Server
 # # from  flask_migrate import Migrate, MigrateCommand
@@ -25,10 +26,15 @@ manager.add_command('server',Server)
 # #     import unittest
 # #     tests = unittest.TestLoader().discover('tests')
 # #     unittest.TextTestRunner(verbosity=2).run(tests)
-
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,
+                db = db,
+                User = User, 
+                Post = Post,
+                Comment = Comment,
+                PostLike = PostLike)
+
 if __name__ == '__main__':
     manager.run()
 # app = Flask(__name__)
