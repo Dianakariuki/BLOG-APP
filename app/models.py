ab4b242fc0a0f88ed0c6362d8ee16eb429e994ab
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-14 22:40:22 +0300

    feature: creating a user class

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from . import db
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    bio = db.Column(db.String())
+    avatar_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    posts = db.relationship("Post",
+                            backref = "user",
+                            lazy = "dynamic")
+    comments = db.relationship("Comment",
+                                backref = "user",
+                                lazy = "dynamic")
+    liked = db.relationship("PostLike",
+                            backref = "user", 
+                            lazy = "dynamic")
