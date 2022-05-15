c40385b5847979b1c478de6d4b2fc81a448cede6
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-15 14:39:06 +0300

    This commit updates the manager.py file

--- manage.py
+++ manage.py
@@ -2,11 +2,7 @@ from flask import Flask
 from app import create_app,db
 from flask_script import Manager, Server
 from app.models import User,Post,Comment,PostLike
-
-
-
-# from flask_script import Manager,Server
-# # from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate, MigrateCommand
 
 
 
@@ -17,8 +13,8 @@ app = create_app('test')
 manager = Manager(app)
 manager.add_command('server',Server)
 
-# # migrate = Migrate(app,db)
-# # manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 @manager.command
