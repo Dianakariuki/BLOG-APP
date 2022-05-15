fdc485813b66b7ebaa94167c557c5d741e1cdae8
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-15 09:55:38 +0300

    This commit imports the two function to the user import

--- app/models.py
+++ app/models.py
@@ -25,6 +25,18 @@ class User(UserMixin, db.Model):
                             lazy = "dynamic")
     
     
+
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     
     
 class Post(db.Model):
